88b776f9460e7347d2895eaafbc71e399780c0f3
Victor Gan <victorgan@gmail.com>
2016-04-09 13:24:55 -0700

    3.3 Actions

--- Hello.elm
+++ Hello.elm
@@ -4,17 +4,31 @@ import Html exposing (Html)
 import Mouse
 
 
-view : Int -> Html
-view count =
-  Html.text (toString count)
+type alias Model = { count : Int }
+
+type Action = NoOp | Increase
+
+-- state
+initialModel : Model
+initialModel = { count = 0 }
 
 
-countSignal : Signal Int
-countSignal =
-  Signal.foldp (\_ state -> state + 1) 0 Mouse.clicks
+-- does nothing but display model
+view : Model -> Html
+view model = Html.text (toString model.count)
+
+updateModelCount : Action -> Model -> Model
+updateModelCount action model = case action of
+    Increase -> { model | count = model.count + 1 }
+    NoOp -> model
+
+actionSignal : Signal.Signal Action
+actionSignal = Signal.map (\_ -> Increase) Mouse.clicks
+
+modelSignal : Signal.Signal Model
+modelSignal = Signal.foldp updateModelCount initialModel actionSignal
 
 
 main : Signal.Signal Html
-main = Signal.map view countSignal
-
--- Signal.map : (a -> result) -> Signal a -> Signal result
+main =
+  Signal.map view modelSignal
