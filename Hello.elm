ad90625362f4a05c27de79e46fa4bb47f6b9dddb
Victor Gan <victorgan@gmail.com>
2016-04-09 13:31:12 -0700

    3.4 multiple Actions

--- Hello.elm
+++ Hello.elm
@@ -2,11 +2,11 @@ module Main (..) where
 
 import Html exposing (Html)
 import Mouse
-
+import Keyboard
 
 type alias Model = { count : Int }
 
-type Action = NoOp | Increase
+type Action = NoOp | MouseClick | KeyPress
 
 -- state
 initialModel : Model
@@ -19,16 +19,28 @@ view model = Html.text (toString model.count)
 
 updateModelCount : Action -> Model -> Model
 updateModelCount action model = case action of
-    Increase -> { model | count = model.count + 1 }
     NoOp -> model
+    MouseClick -> { model | count = model.count + 1 }
+    KeyPress -> { model | count = model.count - 1 }
+
+-----------------------------------------------------------------
+-- Defining Signals.
+-----------------------------------------------------------------
+keyPressSignal : Signal.Signal Action
+keyPressSignal = Signal.map (\_ -> KeyPress) Keyboard.presses
+
+mouseClickSignal : Signal.Signal Action
+mouseClickSignal = Signal.map (\_ -> MouseClick) Mouse.clicks
 
 actionSignal : Signal.Signal Action
-actionSignal = Signal.map (\_ -> Increase) Mouse.clicks
+actionSignal = Signal.merge mouseClickSignal keyPressSignal
 
+-----------------------------------------------------------------
+-- Next State
+-----------------------------------------------------------------
 modelSignal : Signal.Signal Model
 modelSignal = Signal.foldp updateModelCount initialModel actionSignal
 
 
 main : Signal.Signal Html
-main =
-  Signal.map view modelSignal
+main = Signal.map view modelSignal
