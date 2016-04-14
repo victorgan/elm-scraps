82b9a4bc3f47a6947a217ebdf0abc102e9008c43
Victor Gan <victorgan@gmail.com>
2016-04-14 16:13:01 -0700

    testing

--- Hello.elm
+++ Hello.elm
@@ -1,47 +1,64 @@
 module Main (..) where
 
+import String
 import Html exposing (Html)
 import Html.Events as Events
 import StartApp.Simple
 
 
+-------------------------------------------------------------------------------
+-- Model: Data structure and initial state
+-------------------------------------------------------------------------------
 type alias Model =
-  { count : Int 
+  { savingsPerMonth : Float
+  , savingsPerYear : Float
+  , savingsPerDecade : Float
   }
-
-
-type Action
-  = NoOp
-  | Increase
-
 
 initialModel : Model
 initialModel =
-  { count = 0 
+  { savingsPerMonth = 0
+  , savingsPerYear = 0
+  , savingsPerDecade = 0
   }
 
+type Action = NoOp | Increase
 
+-------------------------------------------------------------------------------
+-- View: Only transform to view. Do no calculation
+-------------------------------------------------------------------------------
 view : Signal.Address Action -> Model -> Html
 view address model =
   Html.div
     []
-    [ Html.div [] [ Html.text (toString model.count) ]
-    , Html.button
-        [ Events.onClick address Increase ]
-        [ Html.text "Click" ]
+    [ htmlDivText (String.concat ["If I save $", toString model.savingsPerMonth, " per month"])
+    , htmlDivText (String.concat ["I will save $", toString model.savingsPerYear, " in a year and $", toString model.savingsPerDecade, " in a decade"])
+    , htmlDivText "Based on X% interest and X% inflation I will have an additional $Y due to savings"
+    , Html.button [ Events.onClick address Increase ] [ Html.text "Click" ]
     ]
 
+htmlDivText : String -> Html
+htmlDivText text = Html.div [] [Html.text text]
 
+-------------------------------------------------------------------------------
+-- Update: Functions
+-------------------------------------------------------------------------------
 update : Action -> Model -> Model
-update action model =
-  case action of
-    Increase ->
-      { model | count = model.count + 1 }
+update action = case action of
+    Increase -> increaseSavings 1000
+    _        -> identity
 
-    _ ->
-      model
+increaseSavings : Float -> Model -> Model
+increaseSavings monthSavings model = 
+    { model 
+    | savingsPerMonth = model.savingsPerMonth + monthSavings
+    , savingsPerYear = model.savingsPerYear + monthSavings*12
+    , savingsPerDecade = model.savingsPerDecade + monthSavings*12*10
+    }
 
-
+-------------------------------------------------------------------------------
+-- Main!
+-------------------------------------------------------------------------------
 main : Signal.Signal Html
 main =
   StartApp.Simple.start
